ff3d460abce401a40659fefa3c180059a4a3ecc6
Mashanz <mangatkk@gmail.com>
2021-07-10 19:24:14 +0700

    [UPDATE] profile

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
 console.log(`
 
-Hanjara Cahya Adhyatma (mashanz)
-Software Developer
+==================================================
+[Hanjara CA] Mashanz | The Noob Software Engineer
+==================================================
 
-🔗 GitHub: https://github.com/mashanz
-📃 Website: https://mashanz.com
+My first experience doing programming is when I 
+was at Elementary School (2006). I build 
+calculator and some games. Visual basic is my 
+first language and now I love doing in python. 
+Even I have more than 15 years of experience of 
+programming I still googling the basic syntax when
+I "forgot" about it and re learn.
+
+==================================================
+📃 Linkedin : https://www.linkedin.com/in/mashanz
+🔗 GitHub   : https://github.com/mashanz
+🐥 Twitter  : https://twitter.com/papanberjalan
+🌏 Website  : https://mashanz.com
+==================================================
 
 `);
