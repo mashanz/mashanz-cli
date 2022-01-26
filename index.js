8cb21f6f4204e9160dbb1c08de12c8f9669d20b3
Mashanz <mangatkk@gmail.com>
2022-01-26 09:22:57 +0700

    [UPD] use npmjs publisher

--- index.js
+++ index.js
@@ -2,16 +2,35 @@
 console.log(`
 
 ==================================================
-[Hanjara CA] Mashanz | The Noob Software Engineer
+   Hanjara CA (mashanz) | Full Stack Developer
 ==================================================
 
 My first experience doing programming is when I 
-was at Elementary School (2006). I build 
-calculator and some games. Visual basic is my 
-first language and now I love doing in python. 
-Even I have more than 15 years of experience of 
-programming I still googling the basic syntax when
-I "forgot" about it and re learn.
+was at Elementary School (2006). I build calculator 
+and some games. Visual basic is my first language 
+and now I love doing in python. I have interest 
+with wide variety of technology like IoT, Software 
+Development and Embedded System.
+
+In 2011 I build my first web application, my own 
+CMS and High School Club Online Registration using 
+pure PHP, HTML, CSS, JavaScript, and jQuery 
+Framework using XAMPP stack. 
+
+3 Years after I switch to C++ and Python to build 
+robots. Humanoid Robot Soccer is the most 
+challenging Development in my Undergrad times. We 
+have less than 3 month to build Humanoid Robot 
+that able to play soccer from scratch. Coding the 
+Movements, Coordination, Visual Recognition, Sensor 
+and integrated all system to work simultaneously 
+is not easy but we manage it. 
+
+Recently, I focusing to Software Development 
+because the cost of development for Software is 
+cheaper rather than IoT or Robotics. I can undo/revert 
+the code if there some bugs, but I am unable to 
+undo/revert mainboard if its burned. 
 
 ==================================================
 📃 Linkedin : https://www.linkedin.com/in/mashanz
